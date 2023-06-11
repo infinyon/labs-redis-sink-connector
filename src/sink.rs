b82ae60555653a9053559bbe2b2b69723add4ac2
Alexander Mikhalev <alex@metacortex.engineer>
2023-06-11 21:07:13 +0100

    First working end to end
    Signed-off-by: Alexander Mikhalev <alex@metacortex.engineer>

--- src/sink.rs
+++ src/sink.rs
@@ -1,4 +1,6 @@
+
 use std::borrow::Cow;
+use std::collections::HashMap;
 
 use anyhow::Context;
 use anyhow::Result;
@@ -6,6 +8,7 @@ use async_trait::async_trait;
 
 use fluvio::consumer::Record;
 use fluvio::Offset;
+
 use fluvio_connector_common::{tracing::info, LocalBoxSink, Sink};
 use url::Url;
 
@@ -13,23 +16,24 @@ use redis::JsonAsyncCommands;
 
 use crate::config::RedisConfig;
 
+type KVRecord = HashMap<String, String>;
 #[derive(Debug, Clone)]
 pub(crate) struct RedisSink {
     pub(crate) prefix: String,
     pub(crate) url: Url,
-    pub(crate) to_hash: Option<bool>,
+    pub(crate) operation: Option<String>,
 }
 
 impl RedisSink {
     pub(crate) fn new(config: &RedisConfig) -> Result<Self> {
         let prefix = config.prefix.clone();
         let url = Url::parse(&config.url.resolve()?).context("unable to parse Redis url")?;
-        let to_hash = config.to_hash;
+        let operation = config.operation.clone();
 
         Ok(Self {
             prefix,
             url,
-            to_hash,
+            operation,
         })
     }
 }
@@ -50,16 +54,49 @@ impl Sink<Record> for RedisSink {
             };
             let key = format!("{}:{}", self.prefix, key);
             println!("key: {}", key);
+            let operation = if self.operation.is_some(){
+                let operation=self.operation.clone().unwrap();
+                let operation = operation.to_uppercase();
+                operation
+            }else{
+                String::from("JSON")
+            };
+            let prefix = self.prefix.clone();
             async move {
-                let value = String::from_utf8_lossy(record.value());
-                if let Some(true) = self.to_hash {
-                    info!("Using set");
-                    redis::cmd("SET")
-                        .arg(&[key, value.to_string()])
+                match operation.as_str() {
+                    "SET" => {
+
+                    }
+                    "TS.ADD" => {
+                        info!("Using TS.ADD");
+                        let mut kvs: KVRecord= serde_json::from_slice(record.value())?;
+                        info!("Using Operation: {}", &operation);
+                        println!("kvs: Key {:?}", kvs["key"]);
+                        println!("kvs: Value {:?}", kvs["value"]);
+                        println!("{}",prefix);
+                        let full_key=format!("{}_{}", &prefix, kvs.remove("key").unwrap());
+                        println!("Full key {}",full_key);
+                        let timestamp = if kvs.contains_key("timestamp"){
+                            let timestamp=kvs.remove("timestamp").unwrap();
+                            if timestamp.len()==13{
+                                let timestamp=timestamp.to_string();
+                                timestamp
+                            }else{
+                                "*".to_string()
+                            }
+                        } else{
+                            "*".to_string()
+                        };
+                        redis::cmd("TS.ADD")
+                        .arg(&[full_key, timestamp, kvs["value"].to_string()])
                         .query_async(&mut con)
                         .await?;
-                } else {
-                    con.json_set(key, "$".to_string(), &value).await?;
+                    }
+                    _ => {
+                        info!("Using JSON.SET");
+                        let value = String::from_utf8_lossy(record.value());
+                        con.json_set(key, "$".to_string(), &value).await?;
+                    }
                 }
 
                 Ok::<_, anyhow::Error>(con)
